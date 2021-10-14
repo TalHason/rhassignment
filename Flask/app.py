e48909716190268eaf4d763a20b72406c0bea087
TalHason <92516101+TalHason@users.noreply.github.com>
2021-10-14 18:30:52 +0300

    working

--- Flask/app.py
+++ Flask/app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+import socket
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(port=8000, host="0.0.0.0/0")
+@app.route("/")
+def return_hostname():
+    return "This is app served from {} to {}".format(socket.gethostname(), request.remote_addr)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
